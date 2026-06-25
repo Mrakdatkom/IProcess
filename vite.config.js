11b82e9cb4703eec74b0cd5280332a9d1b9a0e3f
Mrakdatkom <markryanzipagan3@gmail.com>
2026-06-25 08:51:38 +0800

    Fixed path 4

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,21 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        franchise: resolve(__dirname, 'franchise/index.html'),
+        merchant: resolve(__dirname, 'merchant/index.html'),
+      }
+    }
+  },
+  server: {
+    open: true
+  }
 })
